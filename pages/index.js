eda9b953d237077de7b76161902ca06a33e67c47
ILYA STEPANOV <step96.ilman@yandex.ru>
2022-06-05 15:14:19 +0300

    added js functions

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,36 @@
-const editBtn = document.querySelector('.profile__edit-btn');
-const popup = document.querySelector('.popup');
-const closeBtn = document.querySelector('.popup__close-btn');
+let editBtn = document.querySelector('.profile__edit-btn');
+let popup = document.querySelector('.popup');
+let closeBtn = document.querySelector('.popup__close-btn');
+let formElement = popup.querySelector('popup__form');
+let nameInput = popup.querySelector('popup_input_type_name');
+let statusInput = popup.querySelector('popup_input_type_status')
+let profileName = document.querySelector('profile__name');
+let profileStatus = document.querySelector('profile__status');
 
 
-
-editBtn.addEventListener('click', function(){
-  popup.classList.remove('popup_active');
-});
-
-closeBtn.addEventListener('click', function(){
+function openPopup() {
   popup.classList.add('popup_active');
-});
+  nameInput.value = profileName.textContent;
+  statusInput.value = profileStatus.textContent;
+}
 
+
+function closePopup(){
+  popup.classList.remove('popup_active');
+}
+
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  let name = nameInput.value;
+  profileName.textContent = name;
+  let status = statusInput.value;
+  profileStatus.textContent = status;
+  closePopup();
+}
+
+editBtn.addEventListener('click', openPopup);
+
+closeBtn.addEventListener('click', closePopup); 
+
+formElement.addEventListener('submit', formSubmitHandler);
